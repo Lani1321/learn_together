d4f18c7467af10012dabfd28e94e9af2952a92fd
Lani1321 <lani.berry13@gmail.com>
2016-10-31 14:21:34 -0400

    add Tags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031181424) do
+ActiveRecord::Schema.define(version: 20161031181851) do
 
   create_table "resources", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20161031181424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
