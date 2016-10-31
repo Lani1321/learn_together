39e3c1e6dfaf9e7936e427637a3d44aa006ac052
Lani1321 <lani.berry13@gmail.com>
2016-10-31 16:33:43 -0400

    add uid

--- db/migrate/20161031161751_create_users.rb
+++ db/migrate/20161031161751_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string  :name
+      t.integer :uid   
 
       t.timestamps
     end
