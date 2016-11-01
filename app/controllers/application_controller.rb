5586e193580142f40278ea80fbe0fb09879f9835
Lani1321 <lani.berry13@gmail.com>
2016-11-01 13:01:18 -0400

    authenticate user before proceeding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
-class ApplicationController < ActionController::Base
+ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+   # if you have set authenticate_user before protect_from_forgery, your request will result in "Can't verify CSRF token authenticity." 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    # For nested checkboxes
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
+       end
+    end
 end
