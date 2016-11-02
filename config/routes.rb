0864cd287ad6c7c92b380fac1fc9727d50412eb7
Lani1321 <lani.berry13@gmail.com>
2016-11-01 23:44:12 -0400

    change tags routes to topics routes and add routes for devise and omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,25 @@
 Rails.application.routes.draw do
   
+  # devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, 
+                     :path_names => { :sign_up => "register" }
+
   # :resources is the different articles/sites that users post 
   resources :resources
 
   resources :users
 
-  resources :tags
+  resources :topics
 
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'topics#index'
+
+  # I only needed this route when I was using just Oauth
+  # get "/auth/:provider/callback" => "sessions#create"
+
+  get "/signout" => "sessions#destroy", :as => :signout
+  # get '/auth/github/callback' => 'sessions#create'
 
   delete '/resources', to: 'resources#destroy'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
