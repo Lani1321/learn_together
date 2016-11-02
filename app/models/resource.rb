24f0293d7a99a6d18a714141760e7dfaae7ba591
Lani1321 <lani.berry13@gmail.com>
2016-11-01 23:23:05 -0400

    add associations

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,8 @@
 class Resource < ApplicationRecord
   belongs_to :user
+  has_many :resource_topics
+  has_many :topics, :through => :resource_topics
+  accepts_nested_attributes_for :topics, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates :title, presence: true
   validates :link, :format => URI::regexp(%w(http https))
 end
