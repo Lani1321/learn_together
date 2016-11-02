10c9b79cbbf28228ff6faf3650f16a83ae9d8ee9
Lani1321 <lani.berry13@gmail.com>
2016-11-01 23:26:04 -0400

    add devise and omniauth validations and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,31 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+ devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+     :omniauthable, :omniauth_providers => [:github]
+
   has_many :resources
+
+  def self.from_omniauth(auth)
+     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+# delete if doesnt work
+    def self.new_with_session(params, session)
+      if session["devise.user_attributes"]
+        new(session["devise.user_attributes"], without_protection: true) do |user|
+          user.attributes = params
+          user.valid?
+        end
+      else
+        super
+      end
+    end
+
+
 end
