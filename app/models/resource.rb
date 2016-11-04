b3a7de8a749c9c0b83ff0cd59f7e34918cbeee1f
Lani1321 <lani.berry13@gmail.com>
2016-11-03 22:48:19 -0400

    add votes association and method to count votes

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,30 @@
 class Resource < ApplicationRecord
   belongs_to :user
+  has_many :votes
   has_many :resource_topics
   has_many :topics, :through => :resource_topics
   accepts_nested_attributes_for :topics, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates :title, presence: true
   validates :link, :format => URI::regexp(%w(http https))
+  has_many :votes
 
+  def vote_score
+    score = 0
+    votes.each do |vote|
+      score += vote.vote_value
+    end
+    score
+  end
 end
+
+
+  # def self.by_votes
+  #   select('resources.*, coalesce(value, 0) as votes')
+  #   joins('left join resource_votes on resource_id=resources.id')
+  #   order('votes desc')
+  # end
+
+  # def votes 
+  #   read_attributes(:votes) || resource_votes.sum(:value)
+  # end
+
