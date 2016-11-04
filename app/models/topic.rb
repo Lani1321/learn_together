c050327654293f6ab0af379f9372e143198711c8
Lani1321 <lani.berry13@gmail.com>
2016-11-03 22:49:55 -0400

    fixed indentation

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,8 +2,8 @@ class Topic < ApplicationRecord
   has_many :resource_topics
   has_many :resources, :through => :resource_topics
 
-def num_of_resources
-  self.resources.count
-end
+  def num_of_resources
+    self.resources.count
+  end
 
 end
