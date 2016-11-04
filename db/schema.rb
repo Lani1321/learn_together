39cc57dd9fa961d21e7e5a44e85e7729603ff747
Lani1321 <lani.berry13@gmail.com>
2016-11-03 22:59:21 -0400

    add votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101195633) do
+ActiveRecord::Schema.define(version: 20161103235511) do
 
   create_table "resource_topics", force: :cascade do |t|
     t.integer  "resource_id"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 20161101195633) do
     t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "vote_value"
+    t.integer "resource_id"
   end
 
 end
