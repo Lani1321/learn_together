d4837b2add4b6a51a4f614a2efb8b1bb4618c408
Lani1321 <lani.berry13@gmail.com>
2016-11-04 20:43:08 -0400

    add helper methods to add and subtract a vote

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -15,7 +15,57 @@ class Resource < ApplicationRecord
     end
     score
   end
+
+  def add_vote
+    score = 0
+    votes.each do |vote|
+      score + vote.vote_value
+    end
+    score
+  end
+
+  def subtract_vote
+    score = 0
+    votes.each do |vote|
+      score - vote.vote_value
+    end
+    score
+  end
+
+
+  def order_by_votes
+    self.votes.count
+  end
+
+
+
+  def no_vote 
+    score = 0
+    score
+  end
+
+
+  def already_voted?(user_id) 
+                    #=> key that represents the column on the votes table
+                              #=>argument we pass through 
+     # if something is there, then it will return an array of vote instances, if not it will be empty
+      if self.votes.where(user_id: user_id).empty?
+        false
+      else
+        true
+      end
+
+  end
+
+  def most_votes
+    @resource = Resource.joins(:votes).select('resources.*, COUNT(votes.id) AS vote_value').group('votes.resources_id').order('vote_value DESC')
+
+  end
+
+
+
 end
+
 
 
   # def self.by_votes
