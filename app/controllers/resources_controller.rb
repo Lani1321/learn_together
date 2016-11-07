9101dc17b036b5b9477a3002ae8791eb19232abb
Lani1321 <lani.berry13@gmail.com>
2016-11-07 11:25:13 -0500

    find topic id and resource id in show action, add admin_user helper method and change redirect to root page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,55 +1,61 @@
 class ResourcesController < ApplicationController
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, except: [:index, :show] #=>This allows a nonuser to see a list of all the resources but only allows users to add resources
-
+ 
 
   def index
-    @resources = Resource.order("title")
-
+    @resources = Resource.all
   end
 
   def show
     # Find the resource we're interested in, passing in params[:id] to get the :id parameter from the request
-    @resource = Resource.find(params[:id])
+    # @resource = Resource.find(params[:id])
+    if params[:topic_id]
+      @topic = Topic.find_by(id: params[:topic_id])
+      if @topic.nil?
+        redirect_to topics_path, alert: "Topic not found"
+      end
+      @resource = @topic.resources.find_by(id: params[:id])
+      if @resource.nil?
+        redirect_to topic_resource_path(@topic), alert: "Resource not found"
+      end
+    else
+      @resource = Resource.find(params[:id]) 
+    end
   end
 
   def upvote
     resource = Resource.find(params[:resource][:resource_id])
-    # binding.pry
     if resource.already_voted?(current_user.id)
-      # binding.pry
       resource.no_vote
       redirect_to :back, notice: "You can't vote for the same resource more than once"
       return
     else 
-      # binding.pry
       resource.votes.create(vote_value: 1, user_id: current_user.id)
     end
-    redirect_to :back, notice: "You've successfully voted for this resource"
+    redirect_to :back, notice: "You've successfully voted for #{resource.title}"
   end
 
   def downvote
     resource = Resource.find(params[:resource][:resource_id])
-    # binding.pry
     if resource.already_voted?(current_user.id)
-      # binding.pry
       resource.no_vote
       redirect_to :back, notice: "You can't vote for the same #{resource.title} more than once"
       return
     else 
-      # binding.pry
       resource.votes.create(vote_value: -1, user_id: current_user.id)
     end
     redirect_to :back, notice: "You've successfully voted for this #{resource.title}"
   end
 
   def create                #=> params[:attribute] contains the attributes I'm interested in
+    @resources = Resource.all
     @resource = current_user.resources.new(resource_params)
     # Saves the model in the database, returns a boolean if it's saved or not
     # if @resource.save fails in this situation, we need to show the form back to the user. 
     if @resource.save
     # Redirect to show action
-      redirect_to @resource
+      redirect_to '/', alert: "You have successfully created '#{@resource.title}'"
     else 
       # We use render instead of redirect_to when save returns false. 
       # The render method is used so that the @resource object is passed back to the new template when it is rendered.
@@ -77,7 +83,7 @@ class ResourcesController < ApplicationController
       redirect_to :back, alert: "You must be the author in order to edit a resource."
     else
       @resource.update(resource_params)
-      redirect_to resource_path(@resource)
+      redirect_to '/', alert: "You have successfully edited your resource"
     end
   end
       
@@ -89,7 +95,7 @@ class ResourcesController < ApplicationController
       redirect_to :back, alert: "You must be the author in order to delete a resource."
     else
       @resource.destroy
-      redirect_to resources_path
+      redirect_to :back, alert: "Your resource has successfully been deleted"
     end
   end
 
@@ -103,5 +109,10 @@ class ResourcesController < ApplicationController
     def resource_params
       params.require(:resource).permit(:title, :link, :topic_ids => [], :topics_attributes => [:name])
     end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 
 end
