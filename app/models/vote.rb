0c84c2ac87db3cc973dd65ac3fe37f7b7f4a2ec8
Lani1321 <lani.berry13@gmail.com>
2016-11-07 11:32:09 -0500

    add helper method to return total votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,13 @@ class Vote < ApplicationRecord
   
   belongs_to :resource
   belongs_to :user
+
+
+
+  def self.total_votes(resource_id)
+    votes = Vote.where(resource_id: resource_id).sum("vote_value")
+  end
+
   
 end
+
