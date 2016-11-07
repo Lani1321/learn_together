ecb3dbdb5767a2e83ce944bdd89668bbf37ad459
Lani1321 <lani.berry13@gmail.com>
2016-11-07 15:36:50 -0500

    remove configuration for precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Rails.application.configure do
-  config.assets.initialize_on_precompile = false
-end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
