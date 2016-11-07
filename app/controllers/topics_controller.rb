8db4a16a7410abb5cb4b9168bcb53d4ae6955aef
Lani1321 <lani.berry13@gmail.com>
2016-11-07 11:26:49 -0500

    remove pry statements and comments

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,10 +6,31 @@ class TopicsController < ApplicationController
   
   def show              #=> learntogether.com/topic/1
     @topic = Topic.find(params[:id])
-    @resources = @topic.resources.joins(:votes).select('resources.*, COUNT(votes.id) AS vote_value').group('votes.resources_id').order('vote_value DESC')
-    # @resource = Resource.joins(:votes).select('resources.*, COUNT(votes.id) AS vote_value').group('votes.resources_id').order('vote_value DESC')
-
-  
+    @resources = @topic.resources.sort{ |a,b| Vote.total_votes(b.id) <=> Vote.total_votes(a.id)}
   end
 
+  def create_resource
+    topic = Topic.find(params[:topic_id])
+    resource = topic.resources.new(new_resource_params)
+    if resource.save
+      redirect_to topic
+    else
+    render 'new_resource'
+  end
+  end 
+
+  def new_resource
+    topic = Topic.find(params[:topic_id])
+    @resource = topic.resources.new
+  end
+
+  private
+
+    def new_resource_params
+        params.require(:resource).permit(:title, :link, :topic_ids => [], :topics_attributes => [:name])
+    end
+
+
 end
+
+
