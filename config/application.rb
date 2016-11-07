16c536b95f303b4d81bd810ddb5866dbfc441cdd
Lani1321 <lani.berry13@gmail.com>
2016-11-07 15:29:26 -0500

    add configuration to get whole asset pipeline working

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
+Rails.application.configure do
+  config.assets.initialize_on_precompile = false
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
