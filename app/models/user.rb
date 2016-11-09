2d5e9de5c3651157e2148578927d96f632c73e03
Lani1321 <lani.berry13@gmail.com>
2016-11-09 14:33:14 -0500

    fixed indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
- devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
      :omniauthable, :omniauth_providers => [:github]
 
-has_many :resources
-has_many :votes
+  has_many :resources
+  has_many :votes
 
   def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
