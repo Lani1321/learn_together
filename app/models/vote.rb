2d27421bf5c04f390f3b3dd75e09d4d4e4b148df
Lani1321 <lani.berry13@gmail.com>
2016-11-08 22:45:23 -0500

    fixed indentation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,12 +3,9 @@ class Vote < ApplicationRecord
   belongs_to :resource
   belongs_to :user
 
-
-
   def self.total_votes(resource_id)
     votes = Vote.where(resource_id: resource_id).sum("vote_value")
   end
-
   
 end
 
